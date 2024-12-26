46baa0baf2db31cef833a44bca0eaf64aff15ea5
Rsyogesh2 <rsyogesh27@gmail.com>
2024-12-26 07:59:34 +0530

    Vendor Query

--- Frontend/src/pages/VendorQuery.js
+++ Frontend/src/pages/VendorQuery.js
@@ -1,4 +1,4 @@
-import React, { useState , useContext} from "react";
+import React, { useState , useContext,useEffect} from "react";
 import { AppContext } from '../context/AppContext';
 import "./VendorQuery.css"; // Import CSS file
 import { TreeSelect } from "antd";
